b79ea26ffaf41c1ee46a013911d0721dca13d28f
Nihanth Charan <Nihanth Charan>
2016-12-01 00:52:12 +0530

    bug

---
+++ db_connect_test.php
@@ -0,0 +1,14 @@
+<?php
+$mysqli = new mysqli('sql202.rf.gd','rfgd_19176149','nihanth007','rfgd_19176149_registrations');
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+echo $mysqli->host_info . "\n";
+
+$mysqli = new mysqli("127.0.0.1", "user", "password", "database", 3306);
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+echo $mysqli->host_info . "\n";
+?>
