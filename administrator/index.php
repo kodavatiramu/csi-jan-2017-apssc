aaefe095174eafa1241e97ff1540813683227337
Nihanth Charan Mutluru <nihanthmbc@outlook.com>
2016-12-22 18:08:51 +0530

    Logout button

---
+++ administrator/index.php
@@ -0,0 +1,6 @@
+<?php
+session_start();
+$db = new mysqli( 'sql202.rf.gd', 'rfgd_19176149', 'nihanth007', 'rfgd_19176149_registrations' );
+if($_SESSION['logged']==1){
+    $username = $_SESSION['']
+}
